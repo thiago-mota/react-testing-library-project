27726921853aecf27f6a2f3348efc4d099bafd1e
Thiago Mota <th.araujomota@gmail.com>
2022-04-13 13:46:59 -0300

    Adiciona requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import About from '../components/About';
+import renderWithRouter from '../renderWithRouter';
+
+describe('2. Testa o componente About.js', () => {
+  it('Testa se a página contém um h2 com o texto About Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const h2 = screen.getByRole('heading', { name: /About Pokédex/i, level: 2 });
+
+    expect(h2).toBeInTheDocument();
+  });
+
+  it('Testa se a página contém dois parágrafos sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const firstParagraph = screen.getByText(/This application simulates a Pokédex, a d/i);
+    const secondParagraph = screen.getByText(/One can filter Pokémons by type, and see/i);
+
+    expect(firstParagraph && secondParagraph).toBeInTheDocument();
+  });
+
+  it('Testa se a página contém a imagem de uma Pokédex',() => {
+    renderWithRouter(<About />);
+
+    const img = screen.getByRole('img');
+
+    expect(img).toHaveProperty('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
