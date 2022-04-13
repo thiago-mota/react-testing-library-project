b36a2c956b46d445cdc482d71e85ecdc28f08c36
Thiago Mota <th.araujomota@gmail.com>
2022-04-13 13:12:06 -0300

    Adiciona requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
@@ -16,9 +17,36 @@ describe('1. Testa o componente App.js', () => {
     expect(favPokemons).toBeInTheDocument();
   });
 
-  // it('Testa se há o redirecionamento para a página inicial / ao clicar em Home', () => {
+  it('Testa se há o redirecionamento para a página inicial / ao clicar em Home', () => {
+    const { history } = renderWithRouter(<App />);
+    const homeLink = screen.getByRole('link', { name: /Home/i });
 
-  // })
+    userEvent.click(homeLink);
+    expect(history.location.pathname).toBe('/');
+  });
+
+  it('Testa se há o redirecionamento para a página About / ao clicar em About', () => {
+    const { history } = renderWithRouter(<App />);
+    const aboutLink = screen.getByRole('link', { name: /About/i });
+
+    userEvent.click(aboutLink);
+    expect(history.location.pathname).toBe('/about');
+  });
+
+  it('Testa redireciona para a página Favorites ao clicar em Favorite Pokemons', () => {
+    const { history } = renderWithRouter(<App />);
+    const favPokemons = screen.getByRole('link', { name: /Favorite Pokémons/i });
+
+    userEvent.click(favPokemons);
+    expect(history.location.pathname).toBe('/favorites');
+  });
+
+  it('Testa se redireciona para a pagina NotFound ao entrar em URL desconhecida', () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push('/404');
+    expect(history.location.pathname).toBe('/404');
+  });
 });
 
 // https://testing-library.com/docs/queries/about#priority
